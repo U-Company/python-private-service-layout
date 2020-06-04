118217b6bf5f92001ad138ef0c7334382d478ba5
Egor <hedgehogues@bk.ru>
2020-06-04 11:00:41 +0300

    Update server.py

--- {{ cookiecutter.service }}/{{ cookiecutter.service }}/__service/server.py
+++ {{ cookiecutter.service }}/{{ cookiecutter.service }}/__service/server.py
@@ -1,7 +1,46 @@
+import threading
+
 import fastapi
 from starlette.middleware import cors
 
 import info
+
+
+class ThreadMutex:
+    def __init__(self, message):
+        """
+        This is server mutex for handlers
+
+        :param message: message of exception
+        """
+        self.__flag = False
+        self.__mutex = threading.Lock()
+        self.__message = message
+
+    def check(self):
+        if self.__flag:
+            raise fastapi.HTTPException(409, detail=self.__message)
+
+    def acquire(self):
+        self.__mutex.acquire()
+        if self.__flag:
+            self.__flag = False
+            self.__mutex.release()
+            raise fastapi.HTTPException(409, detail=self.__message)
+        self.__flag = True
+
+    def release(self):
+        self.__flag = False
+        self.__mutex.release()
+
+
+def check_mutex(l):
+    """
+    :param l: list of mutex for check
+    :return:
+    """
+    for m in l:
+        m.check()
 
 
 class App:
