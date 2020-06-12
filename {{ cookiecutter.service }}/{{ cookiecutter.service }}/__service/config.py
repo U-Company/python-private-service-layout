0a51356d02fa667ff4090ecf30df9f5165a98196
Egor <hedgehogues@bk.ru>
2020-06-12 13:53:38 +0300

    Update config.py

--- {{ cookiecutter.service }}/{{ cookiecutter.service }}/__service/config.py
+++ {{ cookiecutter.service }}/{{ cookiecutter.service }}/__service/config.py
@@ -7,8 +7,8 @@ from vault_client.client import VaultClient
 import info
 
 
-VAULT_ENV = os.environ.get('VAULT_ENV')
-VAULT_ENV_FILE = os.environ.get('VAULT_ENV_FILE')
+VAULT_ENV = os.environ.get('VAULT_ENV', 'LOCAL')
+VAULT_ENV_FILE = os.environ.get('VAULT_ENV_FILE', 'deployments/.envs/local.env')
 vault_client = VaultClient(environ=VAULT_ENV, env_file=VAULT_ENV_FILE)
 
 assert vault_client.is_authenticated, 'Vault client not authenticated'
