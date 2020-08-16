d7798a834d38e8b67bb9a6aa4092b5bb0ba4571c
hedgehogues <hedgehogues@bk.ru>
2020-08-16 18:45:17 +0300

    [feature]. add logging with app, version and environment

--- {{ cookiecutter.service }}/{{ cookiecutter.python_package }}/__cmd/http_.py
+++ {{ cookiecutter.service }}/{{ cookiecutter.python_package }}/__cmd/http_.py
@@ -2,8 +2,10 @@ import uvicorn
 from loguru import logger
 
 from {{ cookiecutter.python_package }}.__service import server, config
+import info
 
 from prometheus_client import start_http_server
+
 
 
 app = server.build_app(config.allow_origins)
@@ -20,5 +22,7 @@ async def health():
 
 if __name__ == "__main__":
     start_http_server(config.prometheus_port)
+    logger.info(f'App: {info.name}; Version: {info.version}')
+    logger.info(f'Environment: {info.VAULT_ENV}')
     logger.info(f'prometheus port: {config.prometheus_port}')
     uvicorn.run(app.app, host='0.0.0.0', port=config.{{ cookiecutter.python_package }}_port)
