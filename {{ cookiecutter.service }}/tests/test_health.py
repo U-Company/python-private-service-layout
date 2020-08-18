15439f279dcc585f74ff3193067b8c62a889bdc5
hedgehogues <hedgehogues@bk.ru>
2020-08-18 13:39:34 +0300

    [feature]. add APIKey to Health

--- {{ cookiecutter.service }}/tests/test_health.py
+++ {{ cookiecutter.service }}/tests/test_health.py
@@ -6,7 +6,7 @@ import tests
 
 @pytest.mark.asyncio
 async def test_health():
-    method = methods.Health()
+    method = methods.Health(api_key=tests.fpredictor_api_key)
     client = tests.client()
     resp, status = await client.request(method)
     assert status == 204
--- {{ cookiecutter.service }}/{{ cookiecutter.python_package }}/methods.py
+++ {{ cookiecutter.service }}/{{ cookiecutter.python_package }}/methods.py
@@ -1,7 +1,38 @@
+import enum
+
 from clients import http
 
 
-class Health(http.Method):
+api_key_name = 'access_token'
+
+
+class Field(enum.Enum):
+    Cookie = 'cookie'
+    Header = 'header'
+    QueryParam = 'query_param'
+
+
+class CookieNotImplemented(Exception):
+    pass
+
+
+class APIKey(http.Method):
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+
+    def __init__(self, *, api_key: str, field: Field = Field.Header):
+        http.Method.__init__(self)
+        if field == Field.Cookie:
+            raise CookieNotImplemented('cookie not implemented')
+        elif field == Field.Header:
+            self.headers[api_key_name] = api_key
+        elif field == Field.QueryParam:
+            self.params[api_key_name] = api_key
+
+
+class Health(APIKey):
     url_ = "/health"
     m_type = "GET"
     headers = {
